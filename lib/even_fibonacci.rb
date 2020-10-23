1818ddc5c3d5500258a4c9399b0e0bc3b83026cd
edensongjbs <edensongjbs@gmail.com>
2020-10-23 16:33:12 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,12 @@
 # Implement your procedural solution here!
+
+def even_fibonacci_sum(lim)
+    allFibs = [1,2]
+    while ((allFibs[allFibs.length-1]+allFibs[allFibs.length-2]) <= lim) do
+        allFibs << allFibs[allFibs.length-1]+allFibs[allFibs.length-2]
+    end
+    puts allFibs
+    allFibs.reduce(0) { |s, e|
+        e%2==0 ? s+e : s
+    }
+end
